7e58e8c02d9e64e993fdd510ee90722af9bf27d4
Mikhail-Balari <balarimikhail@gmail.com>
2023-05-29 12:51:48 -0300

    Se cambia ruta de archivo sweetalert2 en main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import Swal from "sweetalert2";
+import Swal from "../node_modules/sweetalert2/src/sweetalert2";
 
 // Creo un objeto para manejar el almacenamiento en el local storage.
 const storage = {
